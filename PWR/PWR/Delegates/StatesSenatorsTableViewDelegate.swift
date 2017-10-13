5e96505360e5ea828014685583bd90afc33b016e
Amber Spadafora <ahelenespadafora@gmail.com>
2017-10-13 02:05:57 -0700

    added a tableviewdelegate method file for the homeVC's tableview

---
+++ PWR/PWR/Delegates/StatesSenatorsTableViewDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  StatesSenatorsTableViewDelegate.swift
+//  PWR
+//
+//  Created by Amber Spadafora on 10/13/17.
+//  Copyright © 2017 Amber Spadafora. All rights reserved.
+//
+
+import Foundation
