44fb9f1b11725cbf7e6bb383e8ba27ec1228b8e8
Amber Spadafora <ahelenespadafora@gmail.com>
2017-10-13 02:11:05 -0700

    added SenatorTableViewCell class and started to implemented stateTableViewDelegate methods

--- PWR/PWR/Delegates/StatesSenatorsTableViewDelegate.swift
+++ PWR/PWR/Delegates/StatesSenatorsTableViewDelegate.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+class StatesSenatorsTableViewDelegate: NSObject, UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    
+}
---
+++ PWR/PWR/Views/SenatorTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SenatorTableViewCell.swift
+//  PWR
+//
+//  Created by Amber Spadafora on 10/13/17.
+//  Copyright © 2017 Amber Spadafora. All rights reserved.
+//
+
+import UIKit
+
+class SenatorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
