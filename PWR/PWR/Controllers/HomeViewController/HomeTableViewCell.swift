a42ec1df8f4fb69015bb84612188434ce2df3bce
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-10-15 17:41:53 -0400

    started work on button actions in cell

--- PWR/PWR/Controllers/HomeViewController/HomeTableViewCell.swift
+++ PWR/PWR/Controllers/HomeViewController/HomeTableViewCell.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
+// working off of advice here: https://stackoverflow.com/questions/26880526/performing-a-segue-from-a-button-within-a-custom-uitableviewcell
+
+protocol AllowSegueFromCellButton {
+    func callSegue(from button: UIButton)
+}
+
 class HomeTableViewCell: UITableViewCell {
 
+    // display
     @IBOutlet weak var pic: UIImageView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var party: UILabel!
+    
+    // end-user interactive
+    @IBOutlet weak var senatorButton: UIButton!
+    @IBOutlet weak var callButton: UIButton!
+    @IBOutlet weak var webButton: UIButton!
+
+    // properties
+    
+    var delegate: AllowSegueFromCellButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,6 +37,10 @@ class HomeTableViewCell: UITableViewCell {
         pic.layer.borderColor = UIColor.PWRblueDark.cgColor
         pic.layer.borderWidth = 3
         pic.clipsToBounds = true
+        
+        self.senatorButton.addTarget(self, action: #selector(senatorButtonWasTapped(sender:)), for: .touchUpInside)
+        self.callButton.addTarget(self, action: #selector(callButtonWasTapped(sender:)), for: .touchUpInside)
+        self.webButton.addTarget(self, action: #selector(webButtonWasTapped(sender:)), for: .touchUpInside)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -29,4 +49,13 @@ class HomeTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func senatorButtonWasTapped(sender: UIButton) {
+        guard let setDelegate = self.delegate else { return }
+        
+        setDelegate.callSegue(from: sender)
+    }
+    
+    func callButtonWasTapped(sender: UIButton) {}
+    
+    func webButtonWasTapped(sender: UIButton) {}
 }
